52e41ba70293a66af8d1328b12ba3ab765b3bb93
senman <shuxinman@qq.com>
2018-05-29 09:50:27 +0800

    Update show.php
    测试

--- show.php
+++ show.php
@@ -1,8 +1,4 @@
 <?php
 
 require 'vendor/autoload.php';
-
-
-echo Crontab\controllers\console\CrontabController::info();
-echo Crontab\controllers\console\CrontabController::info();
 echo Crontab\models\CronConfig::info();
