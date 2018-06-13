a6c84be2fb1a8c51928360bc3df8b53424365fe8
shuxinman <shuxinman@qq.com>
2018-06-13 08:57:53 +0800

    增加修改时间

--- cron-manager.sql
+++ cron-manager.sql
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS `cron_config` (
   `path` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '运行程序路径',
   `module` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '所属模块',
   `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `last_run_time` timestamp NULL DEFAULT NULL COMMENT '末次执行时间',
   `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` timestamp NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`cron_config_id`),
@@ -27,9 +28,9 @@ CREATE TABLE IF NOT EXISTS `cron_config` (
 
 -- 正在导出表  cron_config 的数据：~2 rows (大约)
 /*!40000 ALTER TABLE `cron_config` DISABLE KEYS */;
-INSERT INTO `cron_config` (`cron_config_id`, `name`, `remark`, `status`, `start_time`, `interval_time`, `type`, `path`, `module`, `sort`, `create_time`) VALUES
-	(1, '发送邮件', '我是测试的邮件群发任务', 0, '2018-05-28 11:53:19', 1, 1, 'email/send', 'Email', 1, '2018-05-28 11:53:19'),
-	(2, '发送短信', '我是测试的短信群发任务', 0, '2018-05-28 11:53:19', 1, 1, 'email/send', 'Notic', 1, '2018-05-28 11:53:19');
+INSERT INTO `cron_config` (`cron_config_id`, `name`, `remark`, `status`, `start_time`, `interval_time`, `type`, `path`, `module`, `sort`, `last_run_time` , `create_time`,`update_time`) VALUES
+	(1, '发送邮件', '我是测试的邮件群发任务', 0, '2018-05-28 11:53:19', 1, 1, 'email/send', 'Email', 1, '2018-05-28 11:53:19', '2018-05-28 11:53:19', '2018-05-28 11:53:19'),
+	(2, '发送短信', '我是测试的短信群发任务', 0, '2018-05-28 11:53:19', 1, 1, 'email/send', 'Notic', 1, '2018-05-28 11:53:19', '2018-05-28 11:53:19', '2018-05-28 11:53:19');
 /*!40000 ALTER TABLE `cron_config` ENABLE KEYS */;
 
 -- 导出  表 cron_log 结构
@@ -46,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `cron_log` (
 
 -- 正在导出表  cron_log 的数据：~1 rows (大约)
 /*!40000 ALTER TABLE `cron_log` DISABLE KEYS */;
-INSERT INTO `cron_log` (`id`, `cron_config_id`, `status`, `remark`, `create_time`, `update_time`) VALUES
+INSERT INTO `cron_log` (`id`, `cron_config_id`, `status`, `remark`, `create_time`) VALUES
 	(1, 1, 0, '正常执行', '2018-05-28 11:55:33', '2018-05-28 11:55:33');
 /*!40000 ALTER TABLE `cron_log` ENABLE KEYS */;
 
